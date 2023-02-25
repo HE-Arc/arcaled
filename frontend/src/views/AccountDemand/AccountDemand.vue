d643dbbf5005200d9c93dbe2191764f61ef8448c
RuiMarcoLoureiro <rui.loureiro@he-arc.ch>
2023-02-25 18:45:39 +0100

    #8 change link to the login, max width for the page and center

--- frontend/src/views/AccountDemand/AccountDemand.vue
+++ frontend/src/views/AccountDemand/AccountDemand.vue
@@ -5,7 +5,7 @@ import AccountDemandForm from "./AccountDemandForm.vue";
 
 <template>
   <q-layout>
-    <q-page-container class="q-pa-md">
+    <q-page-container class="q-pa-md" style="max-width: 1000px; margin: 0 auto">
       <AccountDemandHeader />
       <AccountDemandForm />
 
@@ -13,10 +13,14 @@ import AccountDemandForm from "./AccountDemandForm.vue";
       <div class="row justify-center q-mt-lg">
         <q-btn
           label="J'ai déjà un compte"
-          color="red"
-          to="/login"
+          color="secondary"
+          :to="{
+            name: 'login',
+          }"
           class="text-weight-bold"
           size="lg"
+          flat
+          rounded
         />
       </div>
     </q-page-container>
