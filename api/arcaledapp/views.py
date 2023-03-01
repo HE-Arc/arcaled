1de6254e382d6d07c5cc473d8cf0a9e8f2470095
sebastie.chappuis3 <sebastien.chappuis1@he-arc.ch>
2023-03-01 16:18:20 +0100

    fix: removed permissions to api views

--- api/arcaledapp/views.py
+++ api/arcaledapp/views.py
@@ -7,7 +7,7 @@ from arcaledapp.serializers import *
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
 
 class AccessRequestViewSet(viewsets.ModelViewSet):
     queryset = AccessRequest.objects.all()
@@ -17,19 +17,19 @@ class AccessRequestViewSet(viewsets.ModelViewSet):
 class BranchViewSet(viewsets.ModelViewSet):
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
 
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
