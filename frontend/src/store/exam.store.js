05aefa0e64b411da00dc22ed7d8d1e054c707590
sebastie.chappuis3 <sebastien.chappuis1@he-arc.ch>
2023-03-06 15:00:25 +0100

    fix: updated to match new JSON representation

--- frontend/src/store/exam.store.js
+++ frontend/src/store/exam.store.js
@@ -17,15 +17,35 @@ export const useStore = defineStore(storeName, {
 
       try {
         const response = await axios.get(`${API_LOCATION}/exams/`);
-
-        const data = response.data;
-
-        return data.map(({ lesson: { branch, teacher, year }, content }) => ({
-          branch,
-          teacher,
-          year: new Date(year).getFullYear(),
+        // All exams (with lesson URL)
+        let exams = response.data;
+        let promises = exams.map(async ({ id, lesson: lessonUrl, content }) => ({
+          id,
+          lesson: (await axios.get(lessonUrl)).data,
           content,
         }));
+        // All exams (with lesson data)
+        exams = await Promise.all(promises);
+        promises = exams.map(async ({ id, lesson: { branch: branchUrl, teacher: teacherUrl, year }, content }) => ({
+          id,
+          lesson: {
+            branch: (await axios.get(branchUrl)).data,
+            teacher: (await axios.get(teacherUrl)).data,
+            year: new Date(year).getFullYear(),
+          },
+          content,
+        }));
+        // All exams (with lesson data + branch data + teacher data)
+        exams = await Promise.all(promises);
+        // All exams (lesson flattened to: branch, teacher, year)
+        exams = exams.map(({ id, lesson: { branch, teacher, year }, content }) => ({
+          id,
+          branch,
+          teacher,
+          year,
+          content,
+        }));
+        return exams;
       } catch (error) {
         this.error = error;
       } finally {
